9ba5229bcd66ba261340a6e77f257473c527f8a0
305 <305@305-PC>
2020-04-20 11:36:48 +0900

    first

---
+++ 20200420/src/Ex04.java
@@ -0,0 +1,14 @@
+
+public class Ex04 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+//		String c="";
+		
+		
+		int d = a>b? 1:0;
+		System.out.println("c = "+(a>b? "a가 큽니다":"b가 큽니다"));
+		System.out.println("d = "+d);
+				
+	}
+}
